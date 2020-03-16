9ed2f02d7670d3965f3bb5fab29bded750b673fd
Valeri Mladenov <valeri.mladenov10@gmail.com>
2020-03-16 18:30:19 +0200

    Fix new user issue

--- graphql/resolvers/report.js
+++ graphql/resolvers/report.js
@@ -1,10 +1,7 @@
 const Report = require("../../models/Reports");
 const User = require("../../models/User");
 const Regions = require("../../config/regions.json");
-const {
-  sendReportWithoutPhoto,
-  sendReportWithPhoto,
-} = require("../../util/mails/sendReport");
+const { sendReportWithoutPhoto } = require("../../util/mails/sendReport");
 const getAuthenticatedUser = require("../middlewares/authenticated");
 
 module.exports = {
@@ -19,52 +16,33 @@ module.exports = {
       if (!user) {
         throw new Error("Unauthenticated!");
       }
-      const registerUser = await User.findOne({ email: user.email });
-      if (!registerUser) {
-        const newUser = new User({
-          firstName: user.firstName,
-          lastName: user.lastName,
-          email: user.email,
-          phone: user.phone,
-        });
-        const savedUser = await newUser.add();
-        const newReport = new Report({
-          reporter: savedUser._id,
-          name,
-          address,
-          region,
-          description,
-          photo,
-        });
-        await newReport.add();
-      } else {
-        const newReport = new Report({
-          reporter: registerUser._id,
-          name,
-          address,
-          region,
-          description,
-          photo,
-        });
-        const savedReport = await newReport.add();
-        const ReportData = await Report.findOne({
-          _id: savedReport._id,
-        }).populate("reporter");
-        const Region = Object.keys(Regions)
-          .filter(function(key) {
-            return Regions[key].name === ReportData.region;
-          })
-          .reduce(function(obj, key) {
-            return Regions[key];
-          }, {});
-        sendReportWithoutPhoto(ReportData, Region);
-        // cause of upload problem, photo will be avaible soon
-        // if (ReportData.photo === "") {
-        //   sendReportWithoutPhoto(ReportData, Region);
-        // } else {
-        //   sendReportWithPhoto(ReportData, Region);
-        // }
-      }
+      const newUser = new User({
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+        phone: user.phone,
+      });
+      const savedUser = await newUser.add();
+      const newReport = new Report({
+        reporter: savedUser._id,
+        name,
+        address,
+        region,
+        description,
+        photo,
+      });
+      const newUserReport = await newReport.add();
+      const ReportData = await Report.findOne({
+        _id: newUserReport._id,
+      }).populate("reporter");
+      const Region = Object.keys(Regions)
+        .filter(function(key) {
+          return Regions[key].name === ReportData.region;
+        })
+        .reduce(function(obj, key) {
+          return Regions[key];
+        }, {});
+      sendReportWithoutPhoto(ReportData, Region);
       return "success";
     },
   },
