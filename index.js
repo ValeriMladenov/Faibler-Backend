cafe3b43c8ba83c4f3443be372ae2c706b0d6d37
Viktor Kynchev <v.kynchev@gmail.com>
2020-04-05 02:33:33 +0300

    fix formatting

--- index.js
+++ index.js
@@ -20,4 +20,4 @@ const server = new ApolloServer({
 server.listen(port).then(({ url }) => {
   console.log(`Server ready at ${url}`);
 });
-console.log(`OneTap Backend ready...`);
+console.log(`Faibler Backend ready...`);
