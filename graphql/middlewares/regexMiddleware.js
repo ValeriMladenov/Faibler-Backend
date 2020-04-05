cafee929d26ad8dec2e8e2e998b2ce755294c332
Viktor Kynchev <v.kynchev@gmail.com>
2020-04-05 03:50:46 +0300

    add check before regex

--- graphql/middlewares/regexMiddleware.js
+++ graphql/middlewares/regexMiddleware.js
@@ -1,9 +1,11 @@
 const { INVALID_REGEX } = require("../../const/regex");
 
 module.exports = req => {
+  if (!req.body.variables) return req;
+
   for (const k in req.body.variables) {
-    req.body.variables[k].replace(INVALID_REGEX, '');
+    req.body.variables[k].replace(INVALID_REGEX, "");
   }
 
-  return req
+  return req;
 };
